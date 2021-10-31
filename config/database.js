303d8eb203766d755233685f2f8ec3ac578b6802
Steven Zimmer <webdevzim@gmail.com>
2021-10-31 10:02:06 -0700

    ssl update

--- config/database.js
+++ config/database.js
@@ -15,9 +15,12 @@ module.exports = ({ env }) => {
             database: config.database,
             username: config.user,
             password: config.password,
+            ssl: {
+              rejectUnauthorized: env.bool("DATABASE_SSL_SELF", false), // For self-signed certificates
+            },
           },
           options: {
-            ssl: false,
+            ssl: env.bool("DATABASE_SSL", false),
           },
         },
       },
