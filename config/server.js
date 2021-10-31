2b583f05beb536dd0f95c5a3c19c676f4147cb86
Steven Zimmer <webdevzim@gmail.com>
2021-10-31 09:43:01 -0700

    strapi appy

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '19b0c0c414b4790f0e85e22bb83b2b0e'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '88d01001-8068-432b-a48b-f73f95028031'
+};
